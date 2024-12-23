6418bd22647a3abbdfef3528f122d785f83ff180
贝肯 <13197864+backen@user.noreply.gitee.com>
2024-12-23 21:20:27 +0800

    Site updated: 2024-12-23 21:20:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/23/hello-world/"];function toRandomPost(){
+var posts=["2024/12/23/hello-world/","2024/12/23/1024创新实验室简介/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
